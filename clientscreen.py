c20d98a010e9a34841282c5f27e91708e60fc82c
Jack-Crowley <jcrowley.jjc@gmail.com>
2022-04-01 18:07:17 -0400

    clientscreen

--- clientscreen.py
+++ clientscreen.py
@@ -1,6 +1,6 @@
 import pygame
 from client import Client
-from TicTacToe.tictactoe_gui import TicTacToe
+from Games.TicTacToe.tictactoe_gui import TicTacToe
 
 window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
