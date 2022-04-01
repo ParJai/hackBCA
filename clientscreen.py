c867e72e667110e7e16d18b4e54df82520abf8bf
Jack-Crowley <jcrowley.jjc@gmail.com>
2022-04-01 17:26:35 -0400

    added tictactoe

--- clientscreen.py
+++ clientscreen.py
@@ -1,31 +1,16 @@
 import pygame
 from client import Client
-
+from TicTacToe.tictactoe_gui import TicTacToe
 
 window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
-
-validUsernames = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ '
-validChars = "`123f4567890-=~!@#$% ^&*v()_+qwertyuiop[]\\asdghjxkl'zcbn,./ZXCVBNM<>?ASDFGHJKL:;\"QWERTYUIOP{}|m"
-
-
-MWIDTH, MHEIGHT = window.get_size()
-pixelratio = 1920/MWIDTH
-
 pygame.font.init()
 
 with open('nothing.txt') as data: nstring = data.read().strip()
 
-login = Login(window,clock,pixelratio,validUsernames)
-while login.newScreen != None:
-    if login.newScreen == "register":
-        register = Register(window,clock,pixelratio,validUsernames)
-    login = Login(window,clock,pixelratio,validUsernames)
-username = login.username.textMessage
-password = login.password.textMessage
+tictactoe = TicTacToe(window,clock)
 
-client = Client(username,password)
+client = Client()
 print('connected')
 
-Chatroom(window,clock,pixelratio,validChars,client, username)
 pygame.display.quit()
