1b651e1220c2d921e4fc6c0410331cfd72c69f95
Jack-Crowley <jcrowley.jjc@gmail.com>
2022-04-02 15:26:38 -0400

    blackjack

--- clientscreen.py
+++ clientscreen.py
@@ -5,6 +5,7 @@ from dashboard import Dashboard
 from tempclient import TempClient
 from Games.Nim.NimGUI import Nim
 from Games.TicTacToe.tictactoe_gui import *
+# from Games.Anagrams.anagrams import anagrams
 from Games.Connect_Four.c4GUI import Connect4
 
 SCREEN_WIDTH = 1000
@@ -13,14 +14,19 @@ SCREEN_HEIGHT = 700
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
-
 dashboard = Dashboard(window, clock)
-# tictactoe = TicTacToe(window, clock)
-#nim = Nim(window, clock)
-
-client = Client(dashboard.dest)
-print(dashboard.dest)
-print('connected')
-tempclient = Connect4(window, clock, client)
+while dashboard.dest != "":
+    client = Client(dashboard.dest)
+    if dashboard.dest == 'agm':
+        tempclient = TicTacToe(window, clock, client)
+    elif dashboard.dest == 'ttt':
+        tempclient = TicTacToe(window, clock, client)
+    elif dashboard.dest == 'bj':
+        tempclient = TicTacToe(window, clock, client)
+    elif dashboard.dest == 'nim':
+        tempclient = Nim(window, clock, client)
+    elif dashboard.dest == 'c4':
+        tempclient = Connect4(window, clock, client)
+    dashboard = Dashboard(window, clock)
 
 pygame.display.quit()
