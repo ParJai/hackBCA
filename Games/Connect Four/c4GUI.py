0f820cf3994540137ba7017057d43ef0a3fad609
gerinome <danielgtravels@gmail.com>
2022-04-02 10:57:38 -0400

    Push

--- Games/Connect Four/c4GUI.py
+++ Games/Connect Four/c4GUI.py
@@ -29,6 +29,7 @@ class Connect4:
         self.player = self.newGame()
 
         self.loadCircles()
+        
         while self.run:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
