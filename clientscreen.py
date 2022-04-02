667da44e0b283e94068d433c202d72a754adca77
Parth Jain <parthjain22@gmail.com>
2022-04-02 10:46:31 -0400

    Nim testing

--- clientscreen.py
+++ clientscreen.py
@@ -1,7 +1,8 @@
 import pygame
 from client import Client
 from Games.TicTacToe.tictactoe_gui import TicTacToe
-from dashboard import Dashboard
+from Games.Nim.NimGUI import Nim
+#from dashboard import Dashboard
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 700
@@ -10,8 +11,9 @@ window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 
-dashboard = Dashboard(window,clock)
-tictactoe = TicTacToe(window,clock)
+# dashboard = Dashboard(window, clock)
+# tictactoe = TicTacToe(window, clock)
+nim = Nim(window, clock)
 
 client = Client()
 print('connected')
