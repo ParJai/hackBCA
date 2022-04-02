4634808e9687d89dac1f1768bf0c44917f24c919
Jack-Crowley <jcrowley.jjc@gmail.com>
2022-04-02 11:06:38 -0400

    bruh

--- clientscreen.py
+++ clientscreen.py
@@ -2,6 +2,7 @@ import pygame
 from client import Client
 from Games.TicTacToe.tictactoe_gui import TicTacToe
 from dashboard import Dashboard
+from tempclient import TempClient
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 700
@@ -14,8 +15,7 @@ dashboard = Dashboard(window,clock)
 
 client = Client(dashboard.dest)
 print(dashboard.dest)
-
-
 print('connected')
+tempclient = TempClient(window, clock, client)
 
 pygame.display.quit()
